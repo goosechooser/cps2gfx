80a0429bc3b4698a089142d212b51beec3cd2890
Marshall <xtremebass2k@gmail.com>
2018-06-20 05:33:40 -0700

    Refactored following changes to Sprite

--- pkg/sprite/reader.go
+++ pkg/sprite/reader.go
@@ -1,9 +1,7 @@
 package sprite
 
 import (
-	"fmt"
 	"io"
-	"strconv"
 
 	"github.com/goosechooser/cps2gfx/pkg/tile"
 )
@@ -14,11 +12,10 @@ type reader interface {
 
 // Decoder for Sprite
 type Decoder struct {
-	r        reader
-	tDecoder tile.Decoder
-	BaseTile string
-	// Width, Height int
-	s Sprite
+	tile.Decoder
+	r             reader
+	BaseTile      string
+	Width, Height int
 }
 
 // DecodeOption just an alias for fun
@@ -34,20 +31,20 @@ func BaseTile(bt string) DecodeOption {
 // Width is how long a Sprite is (in Tiles)
 func Width(w int) DecodeOption {
 	return func(d *Decoder) {
-		d.s.Dx = w
+		d.Width = w
 	}
 }
 
 // Height is how tall a Sprite is (in Tiles)
 func Height(h int) DecodeOption {
 	return func(d *Decoder) {
-		d.s.Dy = h
+		d.Height = h
 	}
 }
 
 // NewDecoder is a constructor ofc
 func NewDecoder(r io.ReadSeeker, opts ...DecodeOption) *Decoder {
-	d := &Decoder{r: r, tDecoder: *tile.NewDecoder(r), s: Sprite{}}
+	d := &Decoder{Decoder: *tile.NewDecoder(r), r: r}
 
 	for _, opt := range opts {
 		opt(d)
@@ -56,61 +53,63 @@ func NewDecoder(r io.ReadSeeker, opts ...DecodeOption) *Decoder {
 	return d
 }
 
-// decode handles seeking to the correct spot in the file
-// and reading the data that makes up all the tiles in a Sprite.
-// The actual reading/unpacking is handled by tDecoder
-func (d *Decoder) decode() error {
+// decode handles reading the data that makes up a Sprite.
+// The actual reading/unpacking is handled by tile.Decoder
+func (d *Decoder) decode() (b []byte, err error) {
+	row := make([]tile.Tile, d.Width)
 
-	a, err := strconv.ParseInt(d.BaseTile, 0, 64)
+	for h := 0; h < d.Height; h++ {
+		for w := 0; w < d.Width; w++ {
+			row[w], err = d.Decoder.Decode()
+			if err != nil {
+				return b, err
+			}
+		}
+
+		b = append(b, combineHorizontal(row, d.Width)...)
+		if err = d.nextRow(); err != nil {
+			return b, err
+		}
+	}
+	return b, nil
+}
+
+// To horizontally stitch tiles together you have to structure the bytes as:
+// [tile0-row0] [tile1-row0] ... [tileN-row0]
+// ... ... ...
+// [tile0-rowN] [tile1-rowN] ... [tileN-rowN]
+// this could be accomplished by a variadic interleave u kno
+func combineHorizontal(tiles []tile.Tile, dx int) (b []byte) {
+	tSize := len(tiles[0].Data)
+	rowLength := tiles[0].Size
+	b = make([]byte, dx*tSize)
+
+	for y := 0; y < rowLength; y++ {
+		start := rowLength * y
+		for x, t := range tiles {
+			offset := x * rowLength
+			copy(b[start*dx+offset:start*dx+offset+rowLength], t.Data[start:start+rowLength])
+		}
+	}
+
+	return b
+}
+
+func (d *Decoder) nextRow() (err error) {
+	//rowOffset represents the distance between rows that make up sprites
+	rowOffset := int64(16 * 128)
+	_, err = d.r.Seek(rowOffset, io.SeekCurrent)
 	if err != nil {
 		return err
 	}
-
-	_, err = d.seek(a*128, io.SeekStart)
-	if err != nil {
-		e := fmt.Errorf("%q: @ basetile %q", err, d.BaseTile)
-		return e
-	}
-
-	d.s.Tiles, _ = d.decodeTiles(d.s.Dx, d.s.Dy)
-
 	return nil
-}
-
-func (d *Decoder) seek(offset int64, whence int) (s int64, err error) {
-	s, err = d.r.Seek(offset, whence)
-	if err != nil {
-		return 0, err
-	}
-
-	return s, nil
-}
-
-func (d *Decoder) decodeTiles(width, height int) (tiles []tile.Tile, err error) {
-	tiles = make([]tile.Tile, width*height)
-	//rowOffset represents the distance between rows that make up sprites
-	rowOffset := int64(16 * 128)
-	for h := 0; h < height; h++ {
-		for w := 0; w < width; w++ {
-			tiles[w+(h*width)], _ = d.decodeTile()
-		}
-		_, err = d.seek(rowOffset, io.SeekCurrent)
-		if err != nil {
-			e := fmt.Errorf("%q: @ basetile %q", err, d.BaseTile)
-			return []tile.Tile{}, e
-		}
-	}
-	return tiles, nil
-}
-func (d *Decoder) decodeTile() (tile.Tile, error) {
-	return d.tDecoder.Decode()
 }
 
 // Decode reads a Sprite from the underlying stream
 func (d *Decoder) Decode() (Sprite, error) {
-	if err := d.decode(); err != nil {
+	data, err := d.decode()
+	if err != nil {
 		return Sprite{}, err
 	}
-
-	return d.s, nil
+	return Sprite{Dx: d.Width, Dy: d.Height, Data: data}, nil
 }
